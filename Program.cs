9700884e8a6a67e0b2f6756682cb724985eded56
xahvier <xavierofficial4@gmail.com>
2024-08-08 08:51:55 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("inpute number: ");
+        int number = int.Parse(Console.ReadLine());
+        PrintMultiplicationTable(number);
+    }
+    static void PrintMultiplicationTable(int number)
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine($"{number} x {i} = {number * i}");
+        }
+    }
+}
